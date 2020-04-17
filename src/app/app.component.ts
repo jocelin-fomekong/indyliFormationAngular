5a430f05665603ecc198668c364862d61968aa42
Jocelin Fomekong <jocelin.fomekong@indyli-services.com>
2020-04-17 17:39:17 +0100

    Partie avant l'intégration des services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'indyliFormationAngular';
+  isAuth = false;
+  lastUpdate = new Promise(
+    (resolve) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 4000
+      );
+    }
+  );
+  appreils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    } ,
+    {
+      name: 'Télévision',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'allumé'
+    }
+  ];
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      },4000
+    );
+  }
+  onAllumer(){
+    console.log('On allume tout');
+  }
 }
